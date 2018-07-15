a33b79ecb151fa6b5edd8291195ee1159b21be0e
ElleeB <laura.trager@gmail.com>
2018-07-15 13:00:42 -0400

    Update preferences view success

--- app/controllers/admin/preferences_controller.rb
+++ app/controllers/admin/preferences_controller.rb
@@ -1,5 +1,25 @@
 class Admin::PreferencesController < ApplicationController
   def index
-    @preference = Preference.new(artist_sort_order: "DESC", song_sort_order: "ASC", allow_create_artists: false, allow_create_songs: true)
+    if Preference.all.empty?
+      @preference = Preference.new
+    else
+      @preference = Preference.first
+    end
+  end
+
+  def new
+    @preference = Preference.new
+  end
+
+  def create
+    @preference = Preference.create(song_sort_order: params[:song_sort_order], artist_sort_order: params[:artist_sort_order], allow_create_artists: params[:allow_create_artists], allow_create_songs: params[:allow_create_songs])
+
+    # redirect_to admin_preferences_path
+  end
+
+  def edit
+  end
+
+  def update
   end
 end
